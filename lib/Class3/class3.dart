212e0d9a7a69a99f36707a6af5adb2ab50595840
imam875 <imamhossen983@gmail.com>
2022-12-17 00:25:44 +0600

    class2

---
+++ lib/Class3/class3.dart
@@ -0,0 +1,35 @@
+main(){
+  conditionState();
+}
+
+conditionState(){
+  int a=53;
+
+  if(a>=80){
+    print(" Congratulations! You got A+");
+  }else if(a>=75){
+    print("Your result= A");
+  }else if(a>=70){
+    print("Your Result= A-");
+  }else if(a>=65){
+    print("Your Result= B+");
+  }else if(a>=60){
+    print("Your Result= B");
+  }else if(a>=55){
+    print("Your Result= B-");
+  }else if(a>=50){
+    print("Your Result= C+");
+  }else if(a>=45){
+    print("Your Result= C");
+  }else if(a>=40){
+    print("Your Result= D");
+  }else{
+    print("Fail");
+  }
+
+
+
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+ main(){
+ ///1 Creat a list with 5 elements
+    List list = ["Imam", "Jamal",1,2,false];
+    print(list);
+
+ ///2 Add a new elements.
+   list.add("Bangladesh");
+   print(list);
+
+ ///3 Remove an elements index 2.
+  list.removeAt(2);
+  print(list);
+
+  ///4 Show an example with contains.
+  print(list.contains(true));
+
+ ///5 Add............................
+    int a=5;
+    int b=3;
+    print("Add rsult="+(a+b).toString());
+    ///minus
+    print("Minus result="+(a-b).toString());
+    ///multiply
+    print("Multiply esult="+(a*b).toString());
+    /// divided
+    print("Divided result="+(a/b).toString());
+
+ ///6. Creat a map with 3 elements
+  List<Map<String,dynamic>>map=[
+        {
+     "Name":"Imam",
+     "Age":24
+    },
+    {
+     "Name":"Jamal",
+     "Age":25
+    },
+    {
+     "Name":"Kamal",
+     "Age":26
+    }
+  ];
+
+    print(map[1]["Name"]);
+  /// 7. Add a new elements.
+  map.add({"Name":"Rahim",
+       "Age":30});
+  print(map);
+
+  ///Remove an element from index 2.
+   map.removeAt(2);
+   print(map);
+
+
+}
